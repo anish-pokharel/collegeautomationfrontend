83d7ca09e1ce7131e1ec16a0ba3401fd4cd5aecf
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2024-06-08 00:16:39 +0545

    course implementation

--- src/app/pages/component/course-enroll/course-enroll.component.ts
+++ src/app/pages/component/course-enroll/course-enroll.component.ts
@@ -1,12 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { EnrollmentService } from '../../../core/services/enrollment_service/enrollment.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-course-enroll',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './course-enroll.component.html',
   styleUrl: './course-enroll.component.css'
 })
 export class CourseEnrollComponent {
-
+  showCourseList:any[]=[];
+  constructor(private http:HttpClient, private enrollmentService:EnrollmentService){
+    this.showCourse();
+  }
+showCourse(){
+  this.enrollmentService.getEnrollmentData().subscribe((res)=>{
+    console.log(res);
+    this.showCourseList=res;
+    console.log(this.showCourseList);
+  })
+}
 }
