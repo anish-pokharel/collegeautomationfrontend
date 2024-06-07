6decda3ce4b34307687d8c70d367e776f06a7f6d
Ishan Subedhi <subedishan10@gmail.com>
2024-06-07 20:38:57 +0545

    nothing much

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { WebsiteComponent } from './website/website/website.component';
 
-
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,WebsiteComponent],
+  imports: [CommonModule, RouterOutlet,WebsiteComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'aollege-automation';
+  title = 'college-automation';
 }
